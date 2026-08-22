1fefca5f1c34b29e55dd1e0953230caa24fe390c
Aditya <otaku.adityaaa@gmail.com>
2026-08-22 23:04:55 +0530

    fix: resolve import casing for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import Home from "./pages/home.jsx";
 import Profile from "./pages/Profile.jsx";
 
 function App() {
